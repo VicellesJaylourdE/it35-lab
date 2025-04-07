6b7b50ac365cdfed41d715f34ad2365414dfc500
vicellesjaylourde <jayvicelles2003@gmail.com>
2025-04-07 21:02:45 +0800

    Updates

--- assets/ios.transition-legacy-BuxhcVyK.js
+++ assets/ios.transition-legacy-BuxhcVyK.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Bm7CZAmU.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-DiP31Rlp.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
