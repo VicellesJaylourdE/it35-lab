bc7652fae41d943b10d7f0ad256ea4ea979e9cb4
vicellesjaylourde <jayvicelles2003@gmail.com>
2025-04-10 08:24:02 +0800

    Added feedcontainer on feed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -21,7 +21,6 @@ import Favorites from './home-tabs/Favorites';
 import Feed from './home-tabs/Feed';
 import Search from './home-tabs/Search';
 import About_me from './home-tabs/About_me';
-import Article from '../components/Article';
 import SignUp from './SignUp';
   const Home: React.FC = () => {
 
@@ -49,7 +48,6 @@ import SignUp from './SignUp';
 
           <Route exact path="/it35-lab/app/home/feed" render={Feed} />
           <Route exact path="/it35-lab/app/home/SignUp" component={SignUp} />
-          <Route exact path="/it35-lab/app/home/feed/article/:id" component={Article} />
           <Route exact path="/it35-lab/app/home/search" render={Search} />
           <Route exact path="/it35-lab/app/home/favorites" render={Favorites} />
           <Route exact path="/it35-lab/app/home/About_me" render={About_me} />
