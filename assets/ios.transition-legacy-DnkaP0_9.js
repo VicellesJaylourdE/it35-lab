6f3e36c20263fd755c3ed531a19b22420329107b
VicellesJaylourdE <jayvicelles2003@gmail.com>
2025-04-27 16:51:33 +0800

    Updates

--- assets/ios.transition-legacy-DnkaP0_9.js
+++ assets/ios.transition-legacy-DnkaP0_9.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-LVYUY25G.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-C6-AgqGj.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
