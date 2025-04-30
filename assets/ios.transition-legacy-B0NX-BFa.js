9a5390950cb1b9934b39d5212dc9e130bf8395d4
VicellesJaylourdE <jayvicelles2003@gmail.com>
2025-04-30 21:56:40 +0800

    Updates

--- assets/ios.transition-legacy-B0NX-BFa.js
+++ assets/ios.transition-legacy-B0NX-BFa.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-De3x0aGd.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-N32UFtyx.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
